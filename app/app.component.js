826b3f57fe223a1939d4e72a9eac09ad7644fa52
Kostas Bastas <costasbastas@gmail.com>
2016-10-29 00:26:22 +0300

    .

---
+++ app/app.component.js
@@ -0,0 +1,9 @@
+(function(app){
+    app.AppComponent = ng.core.Component({
+        selector: 'my-app',
+        template: '<h4>Works</h4>'
+    })
+    .Class({
+        constructor: function(){}
+    });
+})(window.app ||(window.app={}));
